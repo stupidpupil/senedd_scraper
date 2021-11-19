bc166185e4761b40a486c992c5fb9161c4f8cd4f
Adam Watkins <adam@stupidpupil.co.uk>
2021-11-19 09:36:33 +0000

    Add TRANSLIT to iconv

--- R/convert_do_gooder_csv_file_encoding.R
+++ R/convert_do_gooder_csv_file_encoding.R
@@ -3,6 +3,6 @@ convert_do_gooder_csv_file_encoding <- function(dgcsv_path){
     # HACK : Some Welsh characters can't be represented in ISO-8859-1
     str_replace_all("ŵ", "w") %>% 
     str_replace_all("ŷ", "y") %>% 
-    iconv(from="UTF-8", to="ISO-8859-1") %>%
+    iconv(from="UTF-8", to="ISO-8859-1//TRANSLIT") %>%
     writeLines(dgcsv_path)
 }
